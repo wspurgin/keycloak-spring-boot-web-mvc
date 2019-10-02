b262afa06b53b82f731a7263bc6e71f16c362935
Will Spurgin <wspurgin@credera.com>
2019-10-02 13:24:00 -0500

    Add "workaround" from Keycloak-8725

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -2,15 +2,18 @@ package com.example.demo;
 
 import org.keycloak.adapters.springboot.KeycloakSpringBootProperties;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
@@ -20,8 +23,13 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
  * Contains the security configuration for the extension.
  * 
  */
-@KeycloakConfiguration
-@AutoConfigureAfter(WebMvcAutoConfiguration.class)
+// @KeycloakConfiguration
+// Replace KeycloakConfiguration annotation with this: (per KEYCLOAK-8725)
+@Configuration
+@ComponentScan(
+        basePackageClasses = KeycloakSecurityComponents.class,
+        excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.keycloak.adapters.springsecurity.management.HttpSessionManager"))
+@EnableWebSecurity
 public class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
 
     /* (non-Javadoc)
